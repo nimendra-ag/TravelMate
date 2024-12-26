f2fed6ddba9cf0484bd97c06b39363b9ed76b98f
Nimendra Gunawardana <nimendraamiththa1@gmail.com>
2024-12-27 03:51:10 +0530

    Created the RestaurantReview model

---
+++ backend/models/RestaurantReview.js
@@ -0,0 +1,54 @@
+import { model } from "mongoose";
+const RestaurantReviewModel = model('restaurantReviews',{
+    userName:{
+        type:String,
+        required:true
+    },
+    restaurantId:{
+        type:Number,
+        required:true
+    },
+    overallRating:{
+        type: Number,
+        required:true
+    },
+    foodRating:{
+        type:Number,
+        required:true
+    },
+    serviceRating:{
+        type:Number,
+        required:true
+    },
+    valueRating:{
+        type:Number,
+        required:true
+    },
+    atmosphereRating:{
+        type:Number,
+        required:true
+    },
+    visitDate:{
+        type:String,
+        required:true
+    },
+    familyType:{
+        type:String,
+        required:true
+    },
+    mealType:{
+        type:String,
+        required: true
+    },
+    reviewTitle:{
+        type:String,
+        required: true
+    },
+    reviewBody:{
+        type:String,
+        required: true
+    }
+})
+
+
+export{RestaurantReviewModel}
