b6abeaccb1e04e249651e47ce7c5d28a446459c9
BinzHS <senavirathnabh@gmail.com>
2024-12-28 21:16:35 +0530

    Backend 2

--- backend/controller/GuideController.js
+++ backend/controller/GuideController.js
@@ -1,5 +1,35 @@
 import dotenv from "dotenv";
 dotenv.config({ path: "../.env" });
+import {GuideModel} from "../models/Guide.js"
+
+const AddGuide = async (req, res) => {
+  console.log(req.body);
+  try {
+    let guides = await GuideModel.find({});
+    let id = guides.length > 0 ? guides[guides.length - 1].id + 1 : 1;
+    const guide = new GuideModel({
+      id: id,
+      name: req.body.name,
+      area: req.body.area,
+      languages: req.body.languages,
+      chargesPerDay: req.body.chargesPerDay,
+      description: req.body.description,
+      birthDate: req.body.birthDate,
+      contactNumber: req.body.contactNumber,
+      nic: req.body.nic,
+
+    });
+    await guide.save();
+    res.json({
+        success: true,
+        message: 'Guide added successfully',
+        hotel_name: req.body.name,
+    });
+
+  } catch (error) {
+    console.error("Error saving guide:", error);
+    res.status(500).json({ success: false, error: 'Server Error' });
+  }
 import { GuideModel } from "../models/Guide.js";
 
 // Add a new guide
