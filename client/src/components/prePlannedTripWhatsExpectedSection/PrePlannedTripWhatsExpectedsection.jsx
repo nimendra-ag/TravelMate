436b01f7eb28abe7eb6a76f3d22f6e5a520df666
SiyathmaWedamulla <wedamullasiyathma@gmail.com>
2025-01-27 18:42:26 +0530

    preplanned trips completed

--- client/src/components/prePlannedTripWhatsExpectedSection/PrePlannedTripWhatsExpectedsection.jsx
+++ client/src/components/prePlannedTripWhatsExpectedSection/PrePlannedTripWhatsExpectedsection.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import PrePlannedTripWhatsExpectedCard from '../prePlannedTripWhatsExpectedCard/PrePlannedTripWhatsExpectedCard';
 
-const PrePlannedTripWhatsExpectedsection = () => {
+const PrePlannedTripWhatsExpectedsection = ({whatsExpected, mainActivities}) => {
   // Example data
   const images = [
-   "https://picsum.photos/300/500?random=1",
-    "https://picsum.photos/300/500?random=2",
-    "https://picsum.photos/300/500?random=3",
+   "https://picsum.photos/900/1500?random=1",
+    "https://picsum.photos/900/1500?random=2",
+    "https://picsum.photos/900/1500?random=3",
   ];
 
-  const mainActivities = ["Hiking in Ella", "Camping in Horton Plains", "Sightseeing"];
+  // const mainActivities = ["Hiking in Ella", "Camping in Horton Plains", "Sightseeing"];
 
   return (
     <div className="main-container">
@@ -17,7 +17,7 @@ const PrePlannedTripWhatsExpectedsection = () => {
         <div style={{ flex: 1.2, padding: "0 20px" }}>
           <h2>What's Expected</h2>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            {whatsExpected}
           </p>
         </div>
         <div style={{ flex: 0.8, display: "flex", justifyContent: "center" }}>
